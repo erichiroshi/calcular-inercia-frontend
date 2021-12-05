05caed945302080ce633bc17e6509291b3e17522
erichiroshi <erichiroshi@hotmail.com>
2021-12-04 21:02:09 -0400

    mudando a baseUrl para o Azure APIM

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
     //baseUrl: "http://localhost:8080"
-    baseUrl: "https://inercia-tcc.herokuapp.com"
+    //baseUrl: "https://inercia-tcc.herokuapp.com"
+    baseUrl: "https://apim-testes-eric.azure-api.net/calcular"
 }
